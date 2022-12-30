01742d0f861a536b35eee5d053b08885e9fc0177
Zedster82 <55487742+Zedster82@users.noreply.github.com>
2022-12-30 22:55:12 +0000

    assembly

--- SetExcercises/SetExcercises/main.asm
+++ SetExcercises/SetExcercises/main.asm
@@ -6,30 +6,36 @@ ExitProcess PROTO, dwExitCode: DWORD  ; Exit process prototype
 
 .data ; data segment
 	numbers DWORD 3, 2, 3, 1, 7, 5, 7, 8, 9, 2
+	
 	;
 .code ; code segment
 
 main PROC ; main procedure
 	lea	ebx, numbers
-	mov edi, 0;starting at value 0
+	mov esi, 0;starting at value 0
+
 forloop:
-		mov eax, edi
-		mov ecx, 2
-		mul ecx
+		mov eax, esi;Store i into eax
+		mov ecx, 2;Store the value 2
+		mul ecx; i * 2 stored in eax
 
-		add eax, 1
+		add eax, 1; add 1 to eax
 
-		mov esi, 3
-		div esi
+		mov ecx, 3;store 3 into ecx
+		div ecx;divide eax by ecx(3)
 
-		add eax, edi
+		add eax, esi;add i to eax
 
-		mul esi
+		mov ecx, 3;Store 3 in ecx
+		mul ecx;Multiply eax by 3
+		
+		add eax, [ebx + TYPE numbers * esi]
 
-		mov [ebx + TYPE numbers * edi], eax
-		inc edi
-		cmp edi, 10
-		jne forloop
+		mov [ebx + TYPE numbers * esi], eax;Store the result back into array
+
+		inc esi;Add 1 to counter
+		cmp esi, 10;Compare to 10
+		jne forloop;Jump if not equal
 
 
 	INVOKE ExitProcess, 0 ; call exit function
